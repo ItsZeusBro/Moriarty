1c9f53445706cd1a52f3d6ae8373ace6f8abdc60
Zeus <zeusofolympus1000@gmail.com>
2022-07-13 17:51:49 -0400

    this is interesting, RegXGen demands something sinister of Matchic Spells

---
+++ RegXGen.js
@@ -0,0 +1,10 @@
+class RegXGen{
+    constructor(){
+
+    }
+}
+
+//Basically takes a regex string, and parses it for literals using Matchic Spells
+//Matchic spells should have a function that returns the first match out of a set of potential patterns
+//based on the order in which they are placed in an array
+
